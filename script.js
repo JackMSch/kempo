5b77a62747b00cf1f1f1fe69064daf1146940660
JackMSch <146956988+JackMSch@users.noreply.github.com>
2026-05-31 21:17:22 -0400

    v0.2
    added all techniques

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
 let b = document.getElementById("t");
-let i = document.getElementById("i");
-
 
 const synth = window.speechSynthesis;
 
+const yellow = [
+    "Kimono Grab",
+    "Blocking the Kick A",
+    "Blocking the Kick B",
+    "Headlock A",
+    "Headlock B",
+    "Headlock C",
+    "Pushing the Circle A",
+    "Pushing the Circle B",
+    "Lever A",
+    "Lever B",
+    "Crashing Elbows"
+];
+const orange = [
+    "Crash of the Eagle",
+    "Tackle Technique 1",
+    "Tackle Technique 2",
+    "Tackle Technique 3",
+    "Tackle Technique 4",
+    "Rising Elbow",
+    "Dancer",
+    "Scimitar",
+    "Rocking Elbow",
+    "Windmill Guard",
+    "Aiming the Spear",
+    "Flowing Hands",
+    "Spinning from the Sun"
+];
+const purple = [
+    "Spiraling Wrists",
+    "Crane Leap",
+    "Drawbridge A",
+    "Drawbridge B",
+    "Kung Fu Cross",
+    "Divided Swords",
+    "Gift",
+    "Crossing Guard",
+    "Attack from the Temple A",
+    "Attack from the Temple B",
+    "Crouching Falcon",
+    "Wingbreak"
+];
+const blue = [
+    "Darkness",
+    "Sweeping Arm Hook",
+    "Flashing Wings",
+    "Sleeper",
+    "U-Punch",
+    "Five Swords",
+    "Clawing Panther",
+    "Spear of Jade",
+    "Double Blades",
+    "Plucking a Bird from the Sky"
+];
+const green = [
+    "Knee Sweep",
+    "Silk Wind",
+    "Grasping Talon",
+    "Rising Kick",
+    "Crane Kick",
+    "Mantis",
+    "Raising the Shield",
+    "Snapping Twig",
+    "Arching Blades",
+    "Blocking the Sun"
+];
+
+const allBelts = [yellow,orange,purple,blue,green];
 
 b.addEventListener("click", function() {
-    let utterThis = new SpeechSynthesisUtterance(i.value);
+    let belt = allBelts[parseInt(Math.random()*allBelts.length)];
+    let utterThis = new SpeechSynthesisUtterance(belt[parseInt(Math.random()*belt.length)]);
     synth.speak(utterThis);
 });
